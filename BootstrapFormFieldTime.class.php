83f6e4f8b264f4c9c8e3433ca8458692d30ea1e8
Matthew Roberts <matthew.roberts@sk.lung.ca>
2017-11-29 16:40:34 -0600

    html5 specific fields in form class

---
+++ BootstrapFormFieldTime.class.php
@@ -0,0 +1,29 @@
+<?php
+namespace ui;
+require_once(__DIR__ . '/BootstrapFormField.class.php');
+/*
+This class extends BootstrapFormField class to implement view() function for time input.
+*/
+
+class BootstrapFormFieldTime extends BootstrapFormField{
+	public function __construct($id = null, $label = null, $value = null){
+		$this->addClass('form-control');
+		
+		parent::__construct($id, $label, $value);
+	}
+	
+	public function view(){
+		$str = "<input type='time' ";
+		$str .= ( !empty($this->class) ? "class='" . implode(' ', $this->class) . "' " : "");
+		$str .= ( !empty($this->id) ? "id='" . $this->id . "' name='" . $this->id . "' " : "");
+		$str .= ( strlen($this->value) > 0 ? "value='" . $this->value . "' " : "");
+		$str .= ( !empty($this->placeholder) ? "placeholder='" . $this->label . "' " : "");
+		$str .= ( !empty($this->style) ? "style='" . $this->style . "' " : "");
+		$str .= ( $this->isDisabled ? "disabled " : "" );
+		$str .= ( strlen($this->helpText) > 0 ? "aria-describedby='" . $this->id . "HelpText' " : "" );
+		$str .= ( !empty($this->additionalAttr) ? $this->additionalAttr . " " : "");
+		$str .= "/>";
+		
+		return $str;
+	}
+}
